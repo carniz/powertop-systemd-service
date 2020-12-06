b241fe0218a22afd398ac9a9487c9c409f4e2dad
Mikael Carneholm <mikael.carneholm@gmail.com>
2020-12-06 17:57:36 +0100

    Use get-version-action

--- .github/prepare-sources.sh
+++ .github/prepare-sources.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export VERSION=${1:-'test'}
+
 mkdir -p .debpkg/etc/systemd/system
 mkdir -p .debpkg/usr/local/bin
 mkdir -p .debpkg/etc/udev/rules.d
@@ -7,3 +9,5 @@ cp powertop.service .debpkg/etc/systemd/system
 cp async-start-powertop .debpkg/usr/local/bin
 cp async-stop-powertop .debpkg/usr/local/bin
 cp power_supply.rules .debpkg/etc/udev/rules.d
+mkdir .debpkg/debian
+cat debian/control.template | envsubst > .debpkg/debian/control
